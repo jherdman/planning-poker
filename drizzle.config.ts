f211e10c3c020e5fbef625d015d13a36b2026d5a
James Herdman <james.herdman@gmail.com>
2025-10-10 07:48:38 -0400

    task: Connect to a local development database

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from"drizzle-kit";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (typeof DATABASE_URL !== "string") {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: DATABASE_URL,
+  },
+});
---
+++ src/db/index.ts
@@ -0,0 +1,25 @@
+import { drizzle } from 'drizzle-orm/bun-sql';
+import { SQL } from 'bun';
+import * as schema from "./schema";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (typeof DATABASE_URL !== "string") {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const client = new SQL(DATABASE_URL);
+const db = drizzle({ client, schema });
+
+export default db;
+
+// @private
+export async function testConnection() {
+  try {
+    return await client`
+      SELECT 1
+    `;
+  } catch (error) {
+    throw new Error("Failed to connect to the database");
+  }
+}
