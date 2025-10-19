8376d0295375b8bf4f717b72e1419dac61d93858
James Herdman <james.herdman@gmail.com>
2025-10-19 11:51:52 -0400

    chore: Remove unused component

--- src/frontend/pages/parties/new.tsx
+++ src/frontend/pages/parties/new.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
+import type { FormEvent } from "react";
 import { useLocation } from "wouter";
 import type { Party } from "@/db/schema/parties";
 
@@ -6,17 +7,21 @@ export default function NewPartyPage() {
 	const [, navigate] = useLocation();
 
 	const { mutate: createNewParty, isPending } = useMutation({
-		mutationFn: (_formData: FormData) =>
-			fetch("/api/parties", {
+		mutationFn: (event: FormEvent<HTMLFormElement>) => {
+			event.preventDefault();
+			event.stopPropagation();
+
+			return fetch("/api/parties", {
 				method: "POST",
-			}).then((res) => res.json()),
+			}).then((res) => res.json());
+		},
 		onSuccess: (data: { party: Party }) => {
 			navigate(`/parties/${data.party.slug}`);
 		},
 	});
 
 	return (
-		<form action={createNewParty}>
+		<form onSubmit={createNewParty}>
 			<button type="submit" disabled={isPending} className="btn btn-primary">
 				<span>Start a new Party</span>
 				{isPending && (
